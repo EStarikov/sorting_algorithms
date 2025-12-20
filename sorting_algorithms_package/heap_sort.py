9a6be78c87e578ead4316856047f40d88d814777
Egor Starikov <egor.starikov2007@gmail.com>
2025-12-20 12:25:40 +0300

    опечатка

--- sorting_algorithms_package/heap_sort.py
+++ sorting_algorithms_package/heap_sort.py
@@ -14,7 +14,7 @@ def tree(arr, n, i):
 def heap_sort(arr):
     l = len(arr)
     for i in range(l//2, -1, 1):
-        tree(arr, l, 1)
+        tree(arr, l, i)
     while l - 1 > 0:
         arr[0], arr[l-1] = arr[l-1], arr[0]
         l -= 1
